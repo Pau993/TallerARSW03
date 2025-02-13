6d39e6bf98ebaa4f341f902d4cdf2ef05832bcd9
Paulinguis993 <99148906+Pau993@users.noreply.github.com>
2025-02-12 20:10:48 -0500

    Actualización, nos quedó grande pues hacer commit 2

---
+++ src/main/java/edu/eci/arsw/highlandersim/Immortal.java
@@ -0,0 +1,113 @@
+package edu.eci.arsw.highlandersim;
+
+import java.util.List;
+import java.util.Random;
+
+public class Immortal extends Thread {
+
+    private ImmortalUpdateReportCallback updateCallback=null;
+    
+    private int health;
+    
+    private int defaultDamageValue;
+
+    private final List<Immortal> immortalsPopulation;
+
+    private final String name;
+
+    private static volatile boolean paused = false;
+
+    private static final Object pauseLock = new Object();
+
+    private final Random r = new Random(System.currentTimeMillis());
+
+
+    public Immortal(String name, List<Immortal> immortalsPopulation, int health, int defaultDamageValue, ImmortalUpdateReportCallback ucb) {
+        super(name);
+        this.updateCallback=ucb;
+        this.name = name;
+        this.immortalsPopulation = immortalsPopulation;
+        this.health = health;
+        this.defaultDamageValue=defaultDamageValue;
+    }
+
+    public void run() {
+        while (true) {
+            synchronized (pauseLock) {
+                while (paused) {
+                    try {
+                        pauseLock.wait(); 
+                    } catch (InterruptedException e) {
+                        return; 
+                    }
+                }
+            }
+
+            
+            if (immortalsPopulation.size() > 1) {
+                int myIndex = immortalsPopulation.indexOf(this);
+                int nextFighterIndex = r.nextInt(immortalsPopulation.size());
+
+                
+                if (nextFighterIndex == myIndex) {
+                    nextFighterIndex = (nextFighterIndex + 1) % immortalsPopulation.size();
+                }
+
+                Immortal opponent = immortalsPopulation.get(nextFighterIndex);
+                this.fight(opponent);
+            } else {
+                
+                if (immortalsPopulation.size() == 1) {
+                    ControlFrame.notifyWinner(this); 
+                }
+                break; 
+            }
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                return; 
+            }
+        }
+    }
+
+    public static void pause() {
+        paused = true;
+    }
+
+    public static void resumeInmortal() {
+        paused = false;
+        synchronized (pauseLock) {
+            pauseLock.notifyAll(); 
+        }
+    }
+
+    public void fight(Immortal i2) {
+        synchronized (immortalsPopulation) {
+            if (i2.getHealth() > 0) {
+                i2.changeHealth(i2.getHealth() - defaultDamageValue);
+                this.health += defaultDamageValue;
+                updateCallback.processReport("Fight: " + this + " vs " + i2 + "\n");
+            } else {
+            
+                immortalsPopulation.remove(i2);
+                updateCallback.processReport(i2 + " has been removed from the game.\n");
+            }
+        }
+    }
+
+    public void changeHealth(int v) {
+        health = v;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    @Override
+    public String toString() {
+
+        return name + "[" + health + "]";
+    }
+
+}
